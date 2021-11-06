cb6c1dcd2bb75ffea9f8215cae95e235feabb16d
laurenssvg <lvangellecum@gmail.com>
2021-11-06 16:38:08 +0100

    Add my coins button + add check for coin already existing in my coins array

--- components/CoinCard/index.js
+++ components/CoinCard/index.js
@@ -1,16 +1,34 @@
 import React from "react";
 
-const CoinCard = ({ label, image, current_price }) => {
+const CoinCard = ({ coin, setMyCoins, myCoins, showAll }) => {
+  const coinExists = (label) => {
+    return myCoins.some((coin) => coin.label === label);
+  };
+
   return (
-    <article className="flex p-4 border border-gray-200 space-x-4 m-5 rounded-xl shadow-md hover:bg-gray-100 hover:cursor-pointer hover:transition-all ease-in-out hover:translate-x-1">
+    <article
+      onClick={() => {
+        if (!coinExists(coin.label)) {
+          return setMyCoins((prevCoins) => [...prevCoins, coin]);
+        } else {
+          return;
+        }
+      }}
+      className={`flex p-4 border ${
+        coinExists(coin.label) && showAll ? "bg-green-50" : ""
+      } border-gray-200 space-x-4 m-5 rounded-xl shadow-sm hover:bg-blue-50 hover:cursor-pointer hover:transition-all ease-in-out hover:scale-105`}
+    >
       <img
-        src={image}
+        src={coin.image}
         className="flex-none w-14 h-14 object-cover rounded-lg bg-gr"
       />
       <div className="flex justify-between min-w-0 relative flex-auto divide-x">
-        <h2 className="flex text-lg font-semibold items-center">{label}</h2>
+        <h2 className="flex text-lg font-semibold items-center">
+          {coin.label}
+        </h2>
+
         <div className="text-lg font-semibold pr-2 flex items-center pl-5">
-          {current_price.toLocaleString("en-GB", {
+          {coin.current_price.toLocaleString("en-GB", {
             style: "currency",
             currency: "EUR",
             minimumFractionDigits: 2,
@@ -18,6 +36,9 @@ const CoinCard = ({ label, image, current_price }) => {
           })}
         </div>
       </div>
+      {coinExists(coin.label) && showAll ? (
+        <div className="flex text-lg font-semibold items-center">+</div>
+      ) : null}
     </article>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Home = () => {
   const [myCoins, setMyCoins] = useState([]);
-  const [filteredCoins, setFilteredCoins] = useState([]);
+  const [showAll, setShowAll] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
   const { data, error } = useSWR(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false",
@@ -29,22 +29,46 @@ const Home = () => {
       <Header />
       <div className="container mx-auto">
         <input
-          className={`focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-md text-black placeholder-gray-500 border border-gray-200 rounded-lg pl-5 py-3
+          className={`focus:border-light-blue-500 focus:ring-1 focus:ring-gray-300 focus:outline-none w-full text-md text-black placeholder-gray-500 border border-gray-200 rounded-lg pl-5 py-3
           `}
           type="text"
           aria-label="Filter projects"
-          placeholder="Search coins.."
+          placeholder={`Search ${!showAll ? "my " : ""}coins..`}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        {coins
+        <div className="flex m-5">
+          <button
+            onClick={() => setShowAll(true)}
+            className={`w-1/2 flex items-center justify-center rounded-full ${
+              showAll ? "bg-blue-50" : ""
+            } bg-white font-semibold p-5 m-2 hover:bg-blue-50 hover:cursor-pointer hover:transition-all ease-in-out hover:scale-105`}
+          >
+            All coins
+          </button>
+          <button
+            onClick={() => setShowAll(false)}
+            className={`w-1/2 flex items-center justify-center rounded-full ${
+              !showAll ? "bg-blue-50" : ""
+            } bg-white font-semibold p-5 m-2 hover:bg-blue-50 hover:cursor-pointer hover:transition-all ease-in-out hover:scale-105`}
+          >
+            My coins ({myCoins.length})
+          </button>
+        </div>
+        {(showAll ? coins : myCoins)
           .filter(
             (coin) =>
               coin.label.toLowerCase().includes(searchQuery) ||
               coin.value.toLowerCase().includes(searchQuery)
           )
-          .map((filteredCoin) => (
-            <CoinCard key={filteredCoin.value} {...filteredCoin} />
+          .map((filteredCoin, index) => (
+            <CoinCard
+              key={index}
+              coin={filteredCoin}
+              setMyCoins={setMyCoins}
+              myCoins={myCoins}
+              showAll={showAll}
+            />
           ))}
       </div>
     </div>
