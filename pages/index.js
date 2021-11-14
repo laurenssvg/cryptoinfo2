6c9df5bef9f69d2a5a5acb9d790e375ff00629a6
lvg <l.vangellecum@gmail.com>
2021-11-15 00:27:15 +0100

    Add debounced query

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import useCoins from "../hooks/useCoins";
 import useGlobalData from "../hooks/useGlobalData";
 import CoinCard from "../components/CoinCard";
 import SearchBar from "../components/SearchBar";
+import { useDebounce } from "use-debounce";
 
 const Home = () => {
   const [myCoins, setMyCoins] = useState(() => {
@@ -17,6 +18,7 @@ const Home = () => {
   const { coins, isLoading } = useCoins("eur");
   const [filteredCoins, setFilteredCoins] = useState();
   const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedQuery] = useDebounce(searchQuery, 200);
 
   const myFilteredCoins = coins?.filter((coin) =>
     myCoins.find((myCoin) => myCoin.id == coin.id)
@@ -28,13 +30,15 @@ const Home = () => {
 
   if (!isLoading) {
     return (
-      <div className="bg-gray-100 dark:bg-[#252526] min-h-screen w-screen trans">
+      <div className="bg-gray-100 dark:bg-[#252526] min-h-screen w-screen">
         <Header />
         <div className="max-w-3xl mx-auto md:max-w-xl lg:max-w-4xl">
           <SearchBar
             searchQuery={searchQuery}
             setSearchQuery={setSearchQuery}
-            filteredCoins={filteredCoins}
+            placeholder={
+              filteredCoins ? "Search my coins..." : "Search coins..."
+            }
           />
           <div className="flex m-5">
             <button
@@ -73,8 +77,10 @@ const Home = () => {
             {(filteredCoins ? filteredCoins : coins)
               .filter(
                 (coin) =>
-                  coin.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-                  coin.id.toLowerCase().includes(searchQuery.toLowerCase())
+                  coin.name
+                    .toLowerCase()
+                    .includes(debouncedQuery.toLowerCase()) ||
+                  coin.id.toLowerCase().includes(debouncedQuery.toLowerCase())
               )
               .map((filteredCoin) => (
                 <CoinCard
